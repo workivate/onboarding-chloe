6606ed08df52af9a8b7b8c79f6b9ed734b4a2620
chloe hooper <chloe.hooper@morneaushepell.com>
2022-10-19 14:12:17 +0100

    changed back to onboarding

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from chocs.testing import TestClient
-from c_onboarding.application import app  # noqa: E999
+from onboarding.application import app  # noqa: E999
 
 # Suppress pytest errors due to class name
 TestClient.__test__ = False
@@ -8,7 +8,7 @@ TestClient.__test__ = False
 
 @pytest.fixture
 def test_client() -> TestClient:
-    app.use("c_onboarding.healthcheck.handlers")
+    app.use("onboarding.healthcheck.handlers")
 
     client = TestClient(app)
     return client
